e620f8e5fb738e39b88994741d1b894ae92edea3
Oliver Gould <ver@buoyant.io>
2020-07-11 00:24:47 +0000

    daemon fn

--- linkerd/strategy/src/lib.rs
+++ linkerd/strategy/src/lib.rs
@@ -11,6 +11,7 @@ use tonic::{
     body::{Body, BoxBody},
     client::GrpcService,
 };
+use tracing::trace;
 
 #[derive(Clone, Debug)]
 pub struct Client<S, R> {
@@ -18,6 +19,11 @@ pub struct Client<S, R> {
     recover: R,
     context_token: String,
 }
+
+type Init = (
+    api::StrategyResponse,
+    grpc::codec::Streaming<api::StrategyResponse>,
+);
 
 impl<S, R> Client<S, R>
 where
@@ -30,7 +36,7 @@ where
     R: Recover<grpc::Status> + Clone + Send + 'static,
     R::Backoff: Send + Unpin,
 {
-    pub async fn strategy(
+    pub async fn watch(
         &mut self,
         addr: SocketAddr,
     ) -> Result<watch::Receiver<api::StrategyResponse>, Error> {
@@ -39,51 +45,66 @@ where
             context_token: self.context_token.clone(),
         };
 
-        let (strategy, mut stream) = match Self::init(&mut self.service, req.clone()).await {
+        let (strategy, stream) = match Self::init(&mut self.service, req.clone()).await {
             Ok(rsp) => rsp,
             Err(status) => {
                 Self::recover(&mut self.service, req.clone(), &mut self.recover, status).await?
             }
         };
 
-        let (mut tx, rx) = watch::channel(strategy);
-        let mut service = self.service.clone();
-        let mut recover = self.recover.clone();
-        tokio::spawn(async move {
-            loop {
-                match Self::broadcast(&mut tx, &mut stream).await {
-                    Ok(()) => return,
-                    Err(status) => {
-                        futures::select_biased! {
-                            () = tx.closed().fuse() => { return; }
-                            res = Self::recover(&mut service, req.clone(), &mut recover, status).fuse() => {
-                                match res {
-                                    Ok((strategy, new_stream)) => {
-                                        let _ = tx.broadcast(strategy);
-                                        new_stream
-                                    }
-                                    Err(_) => return,
-                                }
-                            }
-                        }
-                    }
-                };
-            }
-        });
+        let (tx, rx) = watch::channel(strategy);
+        tokio::spawn(Self::daemon(
+            self.service.clone(),
+            req,
+            self.recover.clone(),
+            tx,
+            stream,
+        ));
 
         Ok(rx)
     }
 
+    /// Processes an initialized stream/watch, recovering as permitted.
+    async fn daemon(
+        mut service: DestinationClient<S>,
+        req: api::StrategyRequest,
+        mut recover: R,
+        mut tx: watch::Sender<api::StrategyResponse>,
+        mut stream: grpc::codec::Streaming<api::StrategyResponse>,
+    ) {
+        loop {
+            match Self::broadcast(&mut stream, &mut tx).await {
+                Ok(()) => {
+                    trace!("Shutting down; all receivers dropped");
+                    return;
+                }
+                Err(status) => {
+                    futures::select_biased! {
+                        () = tx.closed().fuse() => { return; }
+                        res = Self::recover(&mut service, req.clone(), &mut recover, status).fuse() => {
+                            match res {
+                                Err(error) => {
+                                    trace!(%error, "Watch failed");
+                                    return;
+                                }
+                                Ok((strategy, new_stream)) => {
+                                    // Broadcast the first update from the stream
+                                    let _ = tx.broadcast(strategy);
+                                    stream = new_stream;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /// Retrieves the initial strategy.
     async fn init(
         service: &mut DestinationClient<S>,
         req: api::StrategyRequest,
-    ) -> Result<
-        (
-            api::StrategyResponse,
-            grpc::codec::Streaming<api::StrategyResponse>,
-        ),
-        grpc::Status,
-    > {
+    ) -> Result<Init, grpc::Status> {
         let mut stream = service.strategy(req.clone()).await?.into_inner();
         match stream.try_next().await {
             Ok(Some(s)) => Ok((s, stream)),
@@ -92,19 +113,18 @@ where
         }
     }
 
+    /// Publishes updates from `responses` to `tx` until either close.
+    ///
+    /// An error is returned if the `responses` stream terminates. Success is
+    /// returned if `tx` is closed.
     async fn recover(
         service: &mut DestinationClient<S>,
         req: api::StrategyRequest,
         recover: &mut R,
         status: grpc::Status,
-    ) -> Result<
-        (
-            api::StrategyResponse,
-            grpc::codec::Streaming<api::StrategyResponse>,
-        ),
-        Error,
-    > {
+    ) -> Result<Init, Error> {
         let mut backoff = recover.recover(status)?;
+
         loop {
             if backoff.try_next().err_into::<Error>().await?.is_none() {
                 return Err(grpc::Status::new(grpc::Code::Ok, "Backoff exhausted").into());
@@ -114,27 +134,30 @@ where
             match res {
                 Ok(rsp) => return Ok(rsp),
                 Err(status) => {
+                    // Reuse the existing backoff instead of resetting.
                     let _ = recover.recover(status)?;
                 }
             }
         }
     }
 
+    /// Publishes updates from `responses` to `tx` until either close.
+    ///
+    /// An error is returned if the `responses` stream terminates. Success is
+    /// returned if `tx` is closed.
     async fn broadcast(
+        responses: &mut grpc::codec::Streaming<api::StrategyResponse>,
         tx: &mut watch::Sender<api::StrategyResponse>,
-        stream: &mut grpc::codec::Streaming<api::StrategyResponse>,
     ) -> Result<(), grpc::Status> {
         loop {
             futures::select_biased! {
                 () = tx.closed().fuse() => {
                     return Ok(());
                 }
-                res = stream.try_next().fuse() => {
+                res = responses.try_next().fuse() => {
                     match res? {
                         Some(strategy) => {
-                            if tx.broadcast(strategy).is_err() {
-                                return Ok(());
-                            }
+                            let _ = tx.broadcast(strategy);
                         }
                         None => {
                             return Err(grpc::Status::new(grpc::Code::Ok, "server closed stream"));
