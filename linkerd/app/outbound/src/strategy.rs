784a5baa21fe2d585fb846739fcfdc0795036748
Oliver Gould <ver@buoyant.io>
2020-07-14 05:22:00 +0000

    sketch

--- linkerd/app/outbound/src/strategy.rs
+++ linkerd/app/outbound/src/strategy.rs
@@ -1,3 +1,5 @@
+#![allow(warnings)]
+
 use futures::prelude::*;
 use linkerd2_app_core::Never;
 use std::{
@@ -5,10 +7,22 @@ use std::{
     pin::Pin,
     task::{Context, Poll},
 };
+use tokio::net::TcpStream;
 
+#[derive(Clone, Debug)]
 pub struct Router {}
 
-pub struct Accept {}
+#[derive(Clone, Debug)]
+pub struct Accept {
+    pub target: SocketAddr,
+    detect: Detect,
+}
+
+#[derive(Clone, Debug)]
+enum Detect {
+    Opaque,
+    Client,
+}
 
 impl tower::Service<SocketAddr> for Router {
     type Response = Accept;
@@ -19,9 +33,44 @@ impl tower::Service<SocketAddr> for Router {
         Poll::Ready(Ok(()))
     }
 
-    fn call(&mut self, addr: SocketAddr) -> Self::Future {
+    fn call(&mut self, target: SocketAddr) -> Self::Future {
         Box::pin(async move {
-            unimplemented!("{:?}", addr);
+            return Ok(Accept {
+                target,
+                detect: Detect::Client,
+            });
+        })
+    }
+}
+
+impl tower::Service<TcpStream> for Accept {
+    type Response = Pin<Box<dyn Future<Output = ()> + Send + 'static>>;
+    type Error = Never;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Never>> + Send + 'static>>;
+
+    fn poll_ready(&mut self, _: &mut Context<'_>) -> Poll<Result<(), Never>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, tcp_stream: TcpStream) -> Self::Future {
+        let detect = self.detect.clone();
+        Box::pin(async move {
+            let process: Pin<Box<dyn Future<Output = ()> + Send + 'static>> = match detect {
+                Detect::Opaque => {
+                    Box::pin(async move {
+                        // TODO forward
+                        drop(tcp_stream);
+                    })
+                }
+                Detect::Client => {
+                    Box::pin(async move {
+                        // TODO sniff  SNI.
+                        // TODO detect HTTP.
+                        drop(tcp_stream);
+                    })
+                }
+            };
+            Ok(process)
         })
     }
 }
