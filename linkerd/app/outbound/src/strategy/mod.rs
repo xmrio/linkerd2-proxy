56199c769798f566f2a51af368467429ae6dcd43
Oliver Gould <ver@buoyant.io>
2020-07-22 17:57:26 +0000

    mock out http1 server

--- linkerd/app/outbound/src/strategy/mod.rs
+++ linkerd/app/outbound/src/strategy/mod.rs
@@ -2,7 +2,8 @@ use crate::endpoint::TcpEndpoint;
 use futures::prelude::*;
 use linkerd2_app_core::{
     config::ProxyConfig,
-    proxy::identity,
+    drain,
+    proxy::{http, identity},
     svc,
     transport::{tls, BoxedIo},
     Error, Never, ProxyMetrics,
@@ -22,29 +23,29 @@ use tracing::{debug, info, warn};
 #[derive(Clone)]
 pub struct Router<S> {
     get_strategy: S,
-    config: Config,
+    inner: Inner,
 }
 
 #[derive(Clone)]
 pub struct Accept {
     strategy: watch::Receiver<Strategy>,
-    config: Config,
+    inner: Inner,
 }
 
 #[derive(Clone)]
-struct Config {
+struct Inner {
     config: ProxyConfig,
     identity: tls::Conditional<identity::Local>,
     metrics: ProxyMetrics,
     rng: SmallRng,
+    drain: drain::Watch,
 }
 
 #[allow(dead_code)]
 #[derive(Copy, Clone, Debug)]
 enum Protocol {
     Unknown,
-    Http1,
-    H2,
+    Http(http::Version),
 }
 
 // The router is shared and its responses are buffered/cached so that multiple
@@ -67,11 +68,11 @@ where
         // TODO dispatch timeout
         let strategy = self.get_strategy.call(target);
 
-        let config = self.config.clone();
+        let inner = self.inner.clone();
         Box::pin(async move {
             let strategy = strategy.await?;
 
-            return Ok(Accept { config, strategy });
+            return Ok(Accept { inner, strategy });
         })
     }
 }
@@ -86,7 +87,7 @@ impl tower::Service<TcpStream> for Accept {
     }
 
     fn call(&mut self, tcp: TcpStream) -> Self::Future {
-        let config = self.config.clone();
+        let inner = self.inner.clone();
         let strategy_rx = self.strategy.clone();
 
         Box::pin(async move {
@@ -95,7 +96,7 @@ impl tower::Service<TcpStream> for Accept {
             // TODO metrics...
             let (protocol, io) = match strategy.detect {
                 Detect::Opaque => (Protocol::Unknown, BoxedIo::new(tcp)),
-                Detect::Client => match config.detect(tcp).await {
+                Detect::Client => match inner.detect(tcp).await {
                     Err(error) => {
                         info!(%error, "Protocol detection error");
                         return Ok(());
@@ -109,13 +110,18 @@ impl tower::Service<TcpStream> for Accept {
                     // There's no need to watch for updates with TCP
                     // streams, since the routing decision is made
                     // instantaneously.
-                    match config.proxy_tcp(strategy, io).await {
+                    match inner.proxy_tcp(strategy, io).await {
                         Err(error) => info!(%error, "Connection closed"),
                         Ok(()) => debug!("Connection closed"),
                     }
                 }
-                (Protocol::Http1, io) => config.proxy_http1(strategy_rx, io).await,
-                (Protocol::H2, _io) => unimplemented!("H2 proxy"),
+                (Protocol::Http(version), io) => {
+                    // bleh
+                    match version {
+                        http::Version::Http1 => inner.proxy_http1(strategy_rx, io).await,
+                        http::Version::H2 => unimplemented!("H2 proxy"),
+                    }
+                }
             }
 
             Ok(())
@@ -123,9 +129,9 @@ impl tower::Service<TcpStream> for Accept {
     }
 }
 
-impl Config {
+impl Inner {
     async fn detect(&self, tcp: TcpStream) -> Result<(Protocol, BoxedIo), Error> {
-        use linkerd2_app_core::{proxy::http::Version, transport::io::Peekable};
+        use linkerd2_app_core::transport::io::Peekable;
 
         // TODO sniff  SNI.
 
@@ -135,11 +141,8 @@ impl Config {
         let io =
             tokio::time::timeout(self.config.detect_protocol_timeout, tcp.peek(8192)).await??;
 
-        let proto = Version::from_prefix(io.prefix().as_ref())
-            .map(|v| match v {
-                Version::Http1 => Protocol::Http1,
-                Version::H2 => Protocol::H2,
-            })
+        let proto = http::Version::from_prefix(io.prefix().as_ref())
+            .map(Protocol::Http)
             .unwrap_or(Protocol::Unknown);
 
         Ok((proto, BoxedIo::new(io)))
@@ -222,11 +225,71 @@ impl Config {
     }
 
     #[allow(warnings)]
-    async fn proxy_http1(mut self, strategy: watch::Receiver<Strategy>, io: BoxedIo) {
+    async fn proxy_http1(self, strategy: watch::Receiver<Strategy>, io: BoxedIo) {
         // TODO
         // - create an HTTP server
         // - dispatches to a service that holds the strategy watch...
         // - buffered/cached...
-        unimplemented!();
+        let http_service = self.http_service().await;
+
+        let mut conn = hyper::server::conn::Http::new()
+            .with_executor(http::trace::Executor::new())
+            .http1_only(true)
+            .serve_connection(io, http_service)
+            .with_upgrades();
+
+        let res = tokio::select! {
+            res = &mut conn => { res }
+            handle = self.drain.signal() => {
+                handle.release_after(conn).await
+            }
+        };
+        match res {
+            Err(error) => info!(%error, "Connection closed"),
+            Ok(()) => info!("Connection closed"),
+        }
+    }
+
+    async fn http_service(
+        &self,
+    ) -> impl tower::Service<
+        http::Request<hyper::Body>,
+        Response = http::Response<http::boxed::Payload>,
+        Error = Error,
+        Future = Pin<
+            Box<
+                dyn Future<Output = Result<http::Response<http::boxed::Payload>, Error>>
+                    + Send
+                    + 'static,
+            >,
+        >,
+    > + Send {
+        // Todo cache the service
+        HttpService
+    }
+}
+
+struct HttpService;
+
+impl tower::Service<http::Request<hyper::Body>> for HttpService {
+    type Response = http::Response<http::boxed::Payload>;
+    type Error = Error;
+    type Future = Pin<
+        Box<
+            dyn Future<Output = Result<http::Response<http::boxed::Payload>, Error>>
+                + Send
+                + 'static,
+        >,
+    >;
+
+    fn poll_ready(&mut self, _: &mut Context<'_>) -> Poll<Result<(), Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, req: http::Request<hyper::Body>) -> Self::Future {
+        Box::pin(async move {
+            let _ = req;
+            unimplemented!();
+        })
     }
 }
